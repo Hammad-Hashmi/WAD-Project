67427104526b0941436e97a9fc8d85a87522492a
Hammad Ali Hashmi <hashmihammadali@gmail.com>
2019-01-07 20:15:01 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+'use strict'
+
+let express = require('express');
+let path = require('path');
+let logger = require('morgan');
+let cookieParser = require('cookie-parser');
+let bodyParser = require('body-parser');
+let exphbs = require('express-handlebars');
+let mongoose = require('mongoose');
+
+let routes = require('./routes/routes');
+
+let app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views/images'))); 
+
+app.engine('.hbs', exphbs({
+	extname: '.hbs',
+	defaultLayout: 'main'
+}));
+app.set('view engine', '.hbs');
+
+let port = process.env.PORT || 8080; 
+
+app.use('/', routes);
+mongoose.connect('mongodb://localhost/ingredients');
+
+app.listen(port);
+console.log('Server listening on port ' + port)
